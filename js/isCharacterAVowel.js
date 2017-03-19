b303f64cfe222c49e5bbb1c128b315e9dd369590
Shirong Foo <dee.sr.foo@gmail.com>
2017-03-19 17:38:44 +0800

    add isCharacterAVowel.js

---
+++ js/isCharacterAVowel.js
@@ -0,0 +1,10 @@
+function isCharacterAVowel(char){
+  if(char === 'a' ||char === 'e' || char === 'i' || char === 'o' || char == 'u'){
+    return true
+  } else {
+    return false
+  }
+}
+
+
+isCharacterAVowel(5)
